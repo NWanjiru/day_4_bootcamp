a2f584432e307b59a93643b20daa9e85612f2150
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-09 15:03:04 +0300

    Add working missing_number.py

--- missing_number_lab/missing_number.py
+++ missing_number_lab/missing_number.py
@@ -1,14 +1,16 @@
 def find_missing(list1,list2):
-
-	list3 = []
-	count =0
-
-	for i in list1:
-		if i in list2:
-			set1 = set(list1)
-			set2 = set(list2)
-			count = set1 - set2
-			count = list(count)
-
-	list3.append(count)
-	return (list3)
+  """" Checks whether a number exists another lists and returns the extra number"""
+  a = 0
+  if list1 == list2:
+    return 0
+  
+  
+  for i in list1:
+    if i not in list2:
+      a = i
+  
+  for i in list2:
+    if i not in list1:
+      a = i
+      
+  return a
